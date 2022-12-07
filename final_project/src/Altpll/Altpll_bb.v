1de6c8564f6ddc6d25162c338d025a0b4e0b1307
PoYang Hsieh <nn14661215@gmail.com>
2022-12-08 00:59:00 +0800

    qsys

---
+++ final_project/src/Altpll/Altpll_bb.v
@@ -0,0 +1,14 @@
+
+module Altpll (
+	altpll_0_c25m_clk,
+	altpll_0_c3p2m_clk,
+	altpll_0_c50k_clk,
+	clk_clk,
+	reset_reset_n);	
+
+	output		altpll_0_c25m_clk;
+	output		altpll_0_c3p2m_clk;
+	output		altpll_0_c50k_clk;
+	input		clk_clk;
+	input		reset_reset_n;
+endmodule
